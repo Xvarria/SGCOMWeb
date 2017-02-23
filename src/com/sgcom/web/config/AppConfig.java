70ecc02b4984e9247d43a4c49cc1096aab484656
xvarria <mchavarria@gmail.com>
2017-02-22 23:40:21 -0600

    Avances en implementación de Hibernate

---
+++ src/com/sgcom/web/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sgcom.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+@ComponentScan(basePackages = "com.sgcom.web.controller")
+public class AppConfig {
+ 
+}
