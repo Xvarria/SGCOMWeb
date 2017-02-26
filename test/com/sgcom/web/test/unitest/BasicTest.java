2c56368620f1151e4f861fbd87a63d9ee6fb1576
xvarria <mchavarria@gmail.com>
2017-02-25 21:47:26 -0600

    Proyecto base funcional con Spring 4.3.3.RELEASE y Hibernate 5.1.0.Final, Log4j, y Junit funcional ejecutado desde el IDE. Aún hay problemas con el Junit desde maven

--- test/com/sgcom/web/test/unitest/BasicTest.java
+++ test/com/sgcom/web/test/unitest/BasicTest.java
@@ -10,21 +10,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.sgcom.web.model.Categoria;
 import com.sgcom.web.service.CategoriaBO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration("file:WebContent/WEB-INF/SGCom-servlet.xml")
 public class BasicTest {
 	
 	@Autowired
-	CategoriaBO categoriaBO;
+	private CategoriaBO categoriaBO;
 	
 	@Test
     public void tesMethod() {
 		try {
-			Collection<Categoria> listaCategorias = categoriaBO.listCategorias();
+			Collection<Categoria> listaCategorias = this.categoriaBO.listCategorias();
 			assertNotNull("Retorno de Categorias no NULO", listaCategorias);							
 		} catch (Exception e) {
 			System.err.println("Excepción durante Consulta de Categorias");
